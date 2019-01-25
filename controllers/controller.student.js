b77b4f9159e3c0505670176657b2fa4c11f1dfe0
SRAVYA KANCHARLA <35503094+SRAVYAKANCHARLA@users.noreply.github.com>
2019-01-25 12:29:06 -0600

    Add function for shuffling codewords and assigning to student

--- controllers/controller.student.js
+++ controllers/controller.student.js
@@ -12,3 +12,16 @@ let getstudentDetails = (req,res) => {
         }
     } 
 module.exports.getstudentDetails = getstudentDetails;
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+module.exports.addCourseStudent = addCourseStudent;

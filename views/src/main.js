04bcef790eca28070eac86aa45130684cab713bd
Naveen Kumar Chandaluri <naveenkumarchandaluri@gmail.com>
2019-02-15 16:29:33 -0600

    Implemented toggle for active & Inactive courses

--- views/src/main.js
+++ views/src/main.js
@@ -11,6 +11,7 @@ import {store} from './store'
 import 'font-awesome/css/font-awesome.min.css'
 import VueSweetalert2 from 'vue-sweetalert2'
 import BootstrapVue from 'bootstrap-vue'
+import ToggleButton from 'vue-js-toggle-button'
 
 window.axios = axios.create({
   baseURL: process.env.URL,
@@ -19,6 +20,7 @@ window.axios = axios.create({
 window.$ = require('jquery')
 Vue.use(VueSweetalert2)
 Vue.use(Vuelidate)
+Vue.use(ToggleButton)
 Vue.config.productionTip = false
 require('bootstrap')
 
